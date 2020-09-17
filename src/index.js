5cf15e928aa6e23db8ca2caf21e1e5753687a2aa
harld-x <danilyudin723@gmail.com>
2020-09-17 19:40:32 +0700

    routers, pages

--- src/index.js
+++ src/index.js
@@ -1,4 +1,29 @@
 import './main.css'
-import {createStore} from 'redux'
 
-document.write('Hello React/Redux!')
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {createStore, applyMiddleware} from 'redux'
+import {createBrowserHistory} from 'history'
+import thunk from 'redux-thunk'
+import {routerMiddleware, ConnectedRouter} from 'connected-react-router'
+import {composeWithDevTools} from 'redux-devtools-extension'
+import {Provider} from 'react-redux'
+
+import createRootReducer from 'reducers'
+import Layout from 'containers/layout'
+
+const history = createBrowserHistory()
+const middlewares = [thunk, routerMiddleware(history)]
+const store = createStore(
+    createRootReducer(history),
+    composeWithDevTools(applyMiddleware(...middlewares))
+)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <ConnectedRouter history={history}>
+            <Layout />
+        </ConnectedRouter>
+    </Provider>,
+    document.getElementById('root')
+)
