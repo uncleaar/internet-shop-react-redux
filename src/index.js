acefc281bcb94910a0d395d18949843f7efaefb4
harld-x <danilyudin723@gmail.com>
2020-09-20 16:19:10 +0700

    pagination

--- src/index.js
+++ src/index.js
@@ -10,20 +10,20 @@ import {composeWithDevTools} from 'redux-devtools-extension'
 import {Provider} from 'react-redux'
 
 import createRootReducer from 'reducers'
-import Layout from 'containers/layout'
+import routes from 'routes'
 
 const history = createBrowserHistory()
 const middlewares = [thunk, routerMiddleware(history)]
 const store = createStore(
-    createRootReducer(history),
-    composeWithDevTools(applyMiddleware(...middlewares))
+  createRootReducer(history),
+  composeWithDevTools(applyMiddleware(...middlewares))
 )
 
 ReactDOM.render(
-    <Provider store={store}>
-        <ConnectedRouter history={history}>
-            <Layout />
-        </ConnectedRouter>
-    </Provider>,
-    document.getElementById('root')
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      {routes}
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById('root')
 )
